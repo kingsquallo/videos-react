f37e010010770c73c32ac23ff5d22dc7ca501d77
KingSquallo <doanchitaidn@gmail.com>
2020-02-08 20:24:18 +0700

    Updates

---
+++ precache-manifest.a82866cb495a4a622e72dc3396de6236.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5933952da00f07d6f5be9aff364fdf9",
+    "url": "/videos-react/index.html"
+  },
+  {
+    "revision": "09f9806c5b958b5c901c",
+    "url": "/videos-react/static/css/main.a4185c03.chunk.css"
+  },
+  {
+    "revision": "b94677752219bc3c0e7a",
+    "url": "/videos-react/static/js/2.cfe9dd9c.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/videos-react/static/js/2.cfe9dd9c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09f9806c5b958b5c901c",
+    "url": "/videos-react/static/js/main.a70bf4d2.chunk.js"
+  },
+  {
+    "revision": "503614da81f0aef84b0f",
+    "url": "/videos-react/static/js/runtime-main.964f15ca.js"
+  }
+]);
